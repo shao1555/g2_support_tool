d648e56d39fe37acf13f98a9c4e09efb6b216c32
ma34s <masashi.seino@gmail.com>
2014-04-15 17:00:59 +0900

    changes permanent rooting method

--- files/SuperSu/install_su.sh
+++ files/SuperSu/install_su.sh
@@ -8,9 +8,11 @@ if [ ! -f /sdcard/ccs-auditd_org ] ; then
   $bb cp /system/bin/ccs-auditd /sdcard/ccs-auditd_org
 fi
 $bb rm /system/bin/ccs-auditd
-$bb cp /data/local/tmp/unlock_security_module /system/bin/ccs-auditd
+$bb cp /data/local/tmp/unlock_security_module /system/bin/unlock_security_module
+$bb cp /data/local/tmp/device.db /system/etc/device.db
+$bb cp /data/local/tmp/SuperSu/ccs-auditd /system/bin/ccs-auditd
+chmod 755 /system/bin/unlock_security_module
 chmod 755 /system/bin/ccs-auditd
-
 
 $bb mount -o remount,ro /system
 
