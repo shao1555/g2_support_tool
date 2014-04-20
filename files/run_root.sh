0c91e7b39610bf7daced0e55d0ee8177bf4b1a0d
ma34s <masashi.seino@gmail.com>
2014-04-20 19:59:42 +0900

    update binaries

--- files/run_root.sh
+++ files/run_root.sh
@@ -15,6 +15,12 @@ if [ -f /system/xbin/su ] ; then
 	fi
 fi
 cd $C
-#./get_essential_address
+echo -----------------------------
+echo get_essential_address
+./get_essential_address
+echo -----------------------------
+echo unlock_security_module
 ./unlock_security_module
+echo -----------------------------
+echo run_root_shell
 ./run_root_shell -c $1
