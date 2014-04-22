567cb15e757f3a283d19de95bf2c060013a494c4
ma34s <masashi.seino@gmail.com>
2014-04-23 00:38:36 +0900

    Change UI

--- runme.bat
+++ runme.bat
@@ -3,36 +3,32 @@ for /f "tokens=3 delims= " %%a in ('chcp') do set lang=%%a
 if "%lang%" == "932" (set doc=%~dp0doc\jp) else (set doc=%~dp0doc\en)
 SET adb=%~dp0bin\adb.exe
 
+mode con:cols=80 lines=40
 type "%doc%\01_Thanks.txt"
 pause
-
-
 call :adb_push
 
+:menu
+COLOR 0A
+cls
+type "%doc%\08_menu.txt"
+set /P INPUT=Num :
+if "%INPUT%" == "1" call :backup
+if "%INPUT%" == "2" call :install_recovery
+if "%INPUT%" == "3" call :install_su
+if "%INPUT%" == "9" goto finish
 
-type "%doc%\06_backup.txt"
-set /P INPUT=(N/y):  
-if "%INPUT%" == "Y" call :backup
-if "%INPUT%" == "y" call :backup
+goto menu
 
-
-type "%doc%\03_warning.txt"
-set /P INPUT=(N/y):  
-if "%INPUT%"=="Y" call :install_recovery
-if "%INPUT%"=="y" call :install_recovery
-
-type "%doc%\04_warning_su.txt"
-set /P INPUT=(N/y):  
-if "%INPUT%" == "Y" call :install_su
-if "%INPUT%" == "y" call :install_su
-
+::==============================================================
 :finish
+COLOR 07
 call :clean
 type "%doc%\02_End.txt"
 pause
 exit /b
 
-
+::==============================================================
 :adb_push
 %adb% push "%~dp0files\tmproot" /data/local/tmp/
 %adb% shell chmod 755 /data/local/tmp/busybox_file
@@ -41,12 +37,18 @@ exit /b
 %adb% shell chmod 755 /data/local/tmp/unlock_security_module
 %adb% shell chmod 755 /data/local/tmp/run_root_shell
 %adb% shell chmod 755 /data/local/tmp/run_root.sh
-
-
-
 exit /b
 
+::==============================================================
 :backup
+CLS
+COLOR 0B
+type "%doc%\06_backup.txt"
+set /P INPUT=(N/y):  
+if "%INPUT%" == "Y" call :backup_core
+if "%INPUT%" == "y" call :backup_core
+exit /b
+:backup_core
 echo prepear for backup
 %adb% push "%~dp0files\backup" /data/local/tmp/
 %adb% shell chmod 755 /data/local/tmp/backup.sh
@@ -57,10 +59,21 @@ type "%doc%\07_warning_backup.txt"
 echo save to PC
 mkdir %~dp0backups
 adb pull /sdcard/backup/   "%~dp0backups"
+pause
 exit /b
 
 
+::==============================================================
 :install_recovery
+CLS
+COLOR 0D
+type "%doc%\03_warning.txt"
+set /P INPUT=(N/y):  
+if "%INPUT%"=="Y" call :install_recovery_core
+if "%INPUT%"=="y" call :install_recovery_core
+exit /b
+
+:install_recovery_core
 echo prepear for install recovery
 %adb% shell mkdir -p /data/local/tmp/recovery
 %adb% shell mkdir -p /data/local/tmp/loki
@@ -70,22 +83,30 @@ echo prepear for install recovery
 %adb% shell chmod 755 /data/local/tmp/loki/loki.sh
 %adb% shell chmod 755 /data/local/tmp/loki/loki_tool
 
-
 echo start instrall recovery
 %adb% shell /data/local/tmp/run_root.sh /data/local/tmp/loki/loki.sh
 
+pause
 exit /b
 
+::==============================================================
 :install_su
-cls
+CLS
+COLOR 0E
+type "%doc%\04_warning_su.txt"
+set /P INPUT=(N/y):  
+if "%INPUT%" == "Y" call :install_su_core
+if "%INPUT%" == "y" call :install_su_core
+exit /b
+:install_su_core
 echo prepear for gettting the permanent root
 %adb% shell mkdir -p /data/local/tmp/SuperSu
 %adb% push "%~dp0files\SuperSu" /data/local/tmp/SuperSu
 %adb% shell chmod 755 /data/local/tmp/SuperSu/install_su.sh
-
-
+%adb% shell chmod 755 /data/local/tmp/SuperSu/install-busybox.sh
 
 echo install su binaries
+%adb% shell /data/local/tmp/run_root_shell -c "chmod 6755 /data/local/tmp/SuperSu/sed/xbin/daemonsu"
 %adb% shell /data/local/tmp/run_root.sh /data/local/tmp/SuperSu/install_su.sh
 %adb% shell /data/local/tmp/run_root_shell -c "reboot recovery"
 type "%doc%\05_reboot.txt"
@@ -93,9 +114,10 @@ type "%doc%\05_reboot.txt"
 
 echo start instrall SuperSu.apk
 %adb% install "%~dp0files\SuperSu_forHost\eu.chainfire.supersu-193.apk"
+pause
 exit /b
 
-
+::==============================================================
 :clean
 %adb% shell rm /data/local/tmp/busybox_file
 %adb% shell rm /data/local/tmp/device.db
