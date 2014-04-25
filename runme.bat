29f1ef10269bb2bef137402297e08721b48ee02e
ma34s <masashi.seino@gmail.com>
2014-04-26 00:06:19 +0900

    fix error if working include any space characters.

--- runme.bat
+++ runme.bat
@@ -1,17 +1,16 @@
 @echo off
 for /f "tokens=3 delims= " %%a in ('chcp') do set lang=%%a
-if "%lang%" == "932" (set doc=%~dp0doc\jp) else (set doc=%~dp0doc\en)
-SET adb=%~dp0bin\adb.exe
+if "%lang%" == "932" (set doc="%~dp0doc\jp") else (set doc="%~dp0doc\en")
+SET adb="%~dp0bin\adb.exe"
 
 mode con:cols=80 lines=40
-type "%doc%\01_Thanks.txt"
+type %doc%\"01_Thanks.txt"
 pause
 call :adb_push
 
 :menu
 COLOR 0A
-cls
-type "%doc%\08_menu.txt"
+type %doc%\"08_menu.txt"
 set /P INPUT=Num :
 if "%INPUT%" == "1" call :backup
 if "%INPUT%" == "2" call :install_recovery
@@ -24,7 +23,7 @@ goto menu
 :finish
 COLOR 07
 call :clean
-type "%doc%\02_End.txt"
+type %doc%\"02_End.txt"
 pause
 exit /b
 
@@ -43,7 +42,7 @@ exit /b
 :backup
 CLS
 COLOR 0B
-type "%doc%\06_backup.txt"
+type %doc%\"06_backup.txt"
 set /P INPUT=(N/y):  
 if "%INPUT%" == "Y" call :backup_core
 if "%INPUT%" == "y" call :backup_core
@@ -54,7 +53,7 @@ echo prepear for backup
 %adb% shell chmod 755 /data/local/tmp/backup.sh
 
 echo start backup
-type "%doc%\07_warning_backup.txt"
+type %doc%\"07_warning_backup.txt"
 %adb% shell /data/local/tmp/run_root.sh /data/local/tmp/backup.sh
 echo save to PC
 mkdir %~dp0backups
@@ -67,7 +66,7 @@ exit /b
 :install_recovery
 CLS
 COLOR 0D
-type "%doc%\03_warning.txt"
+type %doc%\"03_warning.txt"
 set /P INPUT=(N/y):  
 if "%INPUT%"=="Y" call :install_recovery_core
 if "%INPUT%"=="y" call :install_recovery_core
@@ -93,7 +92,7 @@ exit /b
 :install_su
 CLS
 COLOR 0E
-type "%doc%\04_warning_su.txt"
+type %doc%\"04_warning_su.txt"
 set /P INPUT=(N/y):  
 if "%INPUT%" == "Y" call :install_su_core
 if "%INPUT%" == "y" call :install_su_core
@@ -109,7 +108,7 @@ echo install su binaries
 %adb% shell /data/local/tmp/run_root_shell -c "chmod 6755 /data/local/tmp/SuperSu/sed/xbin/daemonsu"
 %adb% shell /data/local/tmp/run_root.sh /data/local/tmp/SuperSu/install_su.sh
 %adb% shell /data/local/tmp/run_root_shell -c "reboot recovery"
-type "%doc%\05_reboot.txt"
+type %doc%\"05_reboot.txt"
 %adb% wait-for-device
 
 echo start instrall SuperSu.apk
